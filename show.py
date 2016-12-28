89bd4e89e522dc9df938e5c942406aac8a982a2b
Gonzalo Matarrubia <gonzalomatarrubia@hotmail.com>
2016-12-28 17:19:24 +0100

    Codigo comentado

--- show.py
+++ show.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+# -*- coding: cp1252 -*-
 
 # Import modules for CGI handling 
 import cgi, cgitb 
@@ -15,12 +16,17 @@ cursor = db.cursor()
 nombre = form.getvalue('nombre')
 code   = form.getvalue('code')
 code   = int(code)
+
+#Crear claves de acceso. En este caso la clave es la primera letra del nombre en ASCII +100
 key    = ord(nombre[0])+100
+
+#Creamos una lista con los nombres de las tablas de la base de datos
 cursor.execute("SELECT name from sqlite_master WHERE type='table';")
 aux = cursor.fetchall()
 lista = []
 for i in aux:
         lista.append(i[0])
+#Código HTML para iniciar una pagina web
 print "Content-type:text/html\r\n\r\n"
 print "<html>"
 print "<head>"
@@ -28,15 +34,17 @@ print "<title>Lista de deseos:</title>"
 print "</head>"
 print "<body>"
 #print "<h2>Code: %s Key: %s</h2>" % (code, key)  #only for debugging
+#Comprobamos la clave introducida
 if code==key:
     #print "<h2>Son iguales</h2>" #only for debbuging
     print """<form action="/cgi-bin/update.py " method="POST">"""
     for tabla in lista:
-	#if tabla != nombre.lower():
+                #Creamos listas de todas las ideas de cada tabla
 		print "<h2> Lista de %s:</h2>" % (tabla)
     		cursor.execute("SELECT * FROM %s" % (tabla.lower()) )
     		ideas = cursor.fetchall()
     		for i in ideas:
+                        #Imprimimos las listas. Distiguiendo nuestra lista del resto
 			if tabla != nombre.lower():
 				print "<input type=\"checkbox\" name=\"%s\" value=1 >" % (i[0])
 			print  i[0]
@@ -48,12 +56,15 @@ if code==key:
     print """<input type="submit" value="Asignar">"""
     print "</form>"
     db.close()
+    #Cerramos la base de datos. Hemos terminado las operaciones (de lectura)
 else:
+    #Si la clave introducida es incorrecta
     print "<h2>Clave incorrecta.</h2>"
 
+#Creamos un boton para volver al index.html
 print """<form action="http://192.168.1.106" method="POST">
            <input type="submit" value="Volver">
          </form>"""
-
+#Cerramos la estructura de la pagina web
 print "</body>"
 print "</html>"
--- update.py
+++ update.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+# -*- coding: cp1252 -*-
 
 # Import modules for CGI handling 
 import cgi, cgitb 
@@ -14,29 +15,39 @@ cursor = db.cursor()
 # Get data from fields
 nombre = form.getvalue('nombre')
 code   = form.getvalue('code')
+
+#Creamos una lista con los nombre de las tablas
 cursor.execute("SELECT name from sqlite_master WHERE type='table';")
 aux = cursor.fetchall()
 lista = []
 for i in aux:
         lista.append(i[0])
+#Código html para empezar la pagina web
 print "Content-type:text/html\r\n\r\n"
 print "<html>"
 print "<head>"
 print "<title>Lista de deseos:</title>"
 print "</head>"
 print "<body>"
+
 #####print form ##### only for debugging
+#Vamos recorriendo cada tabla de la db
 for tabla in lista:
 	if tabla != nombre.lower():
+                #Creamos una lista con las ideas de cada tabla
 		cursor.execute("SELECT * FROM %s" % (tabla.lower()) )
 		ideas = cursor.fetchall()
 		for i in ideas:
+                        #Si las ideas están en form son porque tenían el bóton activado
 			if i[0] in form:
+                                #Actualizar el valor "asignado" de las ideas seleccionadas
 				cursor.execute("UPDATE \"%s\" set asignado=\"%s\" where idea=\"%s\" " % (tabla, nombre.lower(), i[0]))
+#Guardar y cerrar la db
 db.commit()
 db.close()
 print "<h2> Asignado correcto </h2>"
 
+#Generar botón de vuelta a show.py
 print """<form action="/cgi-bin/show.py?" method="POST">"""
 print "<input type=\"hidden\" name=\"nombre\" value=\"%s\" >" % (nombre)
 print "<input type=\"hidden\" name=\"code\" value=\"%s\" >" % (code)
@@ -44,5 +55,6 @@ print "<input type=\"hidden\" name=\"code\" value=\"%s\" >" % (code)
 print """<input type="submit" value="Volver">"""
 print "</form>"
 
+#Cerrar estructura de página html
 print "</body>"
 print "</html>"
